0a9fafa3f9ca6a71889ec86b67fe66b911e7f548
magnus <magnus@iastate.edu>
2021-12-27 18:30:20 -0600

    Updates

---
+++ _app/pages/blog.svelte-b7ee414f.js
@@ -0,0 +1,5 @@
+import{S as i,i as c,s as l,t as d,U as h,d as o,g as r,f as u,K as s}from"../chunks/vendor-2d195b70.js";function m(a){let t;return{c(){t=d(`
+
+This is some content`),this.h()},l(e){h('[data-svelte="svelte-hfp9t8"]',document.head).forEach(o),t=r(e,`
+
+This is some content`),this.h()},h(){document.title="Blog"},m(e,n){u(e,t,n)},p:s,i:s,o:s,d(e){e&&o(t)}}}class _ extends i{constructor(t){super();c(this,t,null,m,l,{})}}export{_ as default};
