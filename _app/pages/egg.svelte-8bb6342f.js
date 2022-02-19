cd2363d26d2ab13d3a21de4d42d700116d3ec9ff
magnus <magnus@iastate.edu>
2022-02-18 18:47:30 -0600

    Updates

---
+++ _app/pages/egg.svelte-8bb6342f.js
@@ -0,0 +1,1 @@
+import{S as o,i,s as r,t as c,g as l,f,K as s,d as u}from"../chunks/vendor-219f1c8b.js";function d(a){let t;return{c(){t=c("Hi Ron!")},l(e){t=l(e,"Hi Ron!")},m(e,n){f(e,t,n)},p:s,i:s,o:s,d(e){e&&u(t)}}}class p extends o{constructor(t){super();i(this,t,null,d,r,{})}}export{p as default};
