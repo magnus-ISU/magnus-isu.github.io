3becb4172d8bca04d3f07d18e818d2320c26053f
magnusanderson-wk <magnus.anderson@workiva.com>
2025-06-10 15:31:03 -0500

    x

--- src/routes/games/character_sheet/+page.svelte
+++ src/routes/games/character_sheet/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Sheet from '../../../lib/components/character_sheet_2/src/lib/Sheet.svelte';
+	import Sheet from '../../../lib/components/character_sheet/src/lib/Sheet.svelte';
 	import '../../../lib/components/character_sheet_2/src/lib/global.css';
 </script>
 
