5e1b66afdebb04431842df5b766e7d7ac1d93ec9
magnusanderson-wk <magnus.anderson@workiva.com>
2026-05-06 07:28:54 -0500

    cleaner builds

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	build: {
+		reportCompressedSize: false,
+	},
+	logLevel: 'warn',
 	server: {
 		fs: {
 			allow: ['.'],
